84eea071a4079686b734b03e0dd4c5146d55f7aa
srpaul2091 <shadana.paul@gmail.com>
2026-09-08 13:51:06 +0930

    Commit initiated at:  Tuesday, September 08, 2026 at 01:51 PM

--- scripts/02_bootstrap_argocd.py
+++ scripts/02_bootstrap_argocd.py
@@ -153,7 +153,7 @@ GITHUB_USERNAME = prompt("GITHUB_USERNAME", "Your personal GitHub username",
                           "srpaul2091", "ravdsun")
 GITOPS_TOKEN    = prompt_secret("GITOPS_TOKEN",
                                 "GitHub Personal Access Token with read access to gitops",
-                                "github_pat_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                                "github__XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
 default_gitops = os.path.join(DEFAULT_PROJECT_ROOT, "gitops")
 GITOPS_PATH     = prompt("GITOPS_PATH", "Local path to your gitops repo",
